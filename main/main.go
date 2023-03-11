fc6d18c78bae72a116d1bc2a80ffa685fa305e1c
Anastasia Kravtsova <anastasya.kallisto@gmail.com>
2023-03-11 23:19:36 +0600

    Работает. Узнала три вещи
    1) зачем-то нужна папка static
    2) почему-то внутри этой папки невозможно сделать
    разделение на другие папки html, css, js, в итоге все в кучу
    3) работает привязка к css и js внутри html ЛИБО на сервере, ЛИБО в ручном режиме
    Одновременно это работать не будет, нужны разные пути

--- main/main.go
+++ main/main.go
@@ -6,17 +6,18 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "html/MainPage.html")
+		http.ServeFile(w, r, "static/MainPage.html")
+
 	})
 
 	http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
 		name := r.FormValue("name")
-		http.ServeFile(w, r, "html/MainPage.html")
+		http.ServeFile(w, r, "static/MainPage.html")
 		fmt.Fprintf(w, "Hello %s", name)
 	})
-
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js/"))))
+	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe("localhost:8080", nil)
 }
