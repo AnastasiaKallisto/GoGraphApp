d5a4efbf7e0809b436d9f0b303037de189af19ba
Anastasia Kravtsova <anastasya.kallisto@gmail.com>
2023-03-12 22:31:02 +0600

    Передача данных из формы на сервер и обратно
    Пока не понятно, почему, если оставлять этот код
    в стороннем файле(то что с<p>), ничего
    не работает, как будто не видно этот файл
    и как будто нельзя его поменять, хз

--- main/main.go
+++ main/main.go
@@ -5,22 +5,30 @@ import (
 	"net/http"
 )
 
+type QuantityData struct {
+	Quantity string
+}
+
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("static/html/MainPage.html",
 		"static/html/canvasForExactGraph.html",
 		"static/html/dropdownButtonExact.html",
 		"static/html/headerMenu.html",
-		"static/html/textDescription.html")
-	t.ExecuteTemplate(w, "mainPage", nil)
+		"static/html/formForQuantity.html")
+	quantity := r.FormValue("quantity")
+	data := QuantityData{
+		Quantity: quantity,
+	}
+	t.ExecuteTemplate(w, "mainPage", data)
 }
 
 func handleFunc() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", mainPage)
 	http.ListenAndServe("localhost:8080", nil)
 }
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	handleFunc()
 }
