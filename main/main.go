8e024ca58a38cca8e60e01c9fac16ed3f67d3453
Anastasia Kravtsova <anastasya.kallisto@gmail.com>
2023-03-12 14:06:24 +0600

    Разделила на папки html, css, js
    Проблема была в том, что если я перемещаю файл, то
    автоматически исправляются относительные пути внутри
    html файла, а я этого не замечала
    Для сервера один путь, а он ставил как будто я ручками файл открываю

--- main/main.go
+++ main/main.go
@@ -9,13 +9,13 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/MainPage.html")
+		http.ServeFile(w, r, "static/html/MainPage.html")
 
 	})
 
 	http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
 		name := r.FormValue("name")
-		http.ServeFile(w, r, "static/MainPage.html")
+		http.ServeFile(w, r, "static/html/MainPage.html")
 		fmt.Fprintf(w, "Hello %s", name)
 	})
 	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
