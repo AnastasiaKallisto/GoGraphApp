b0dcc05908c1311ce56fc0a4af9c3dcb67c13037
Anastasia Kravtsova <anastasya.kallisto@gmail.com>
2023-03-15 01:37:12 +0600

    Вставила старый код рисования прямоугольника
    Пришла к выводу, что связка go -> html -> script работает
    Но граф от чего-то рисовать не хочет

--- static/js/scriptForDrawing.js
+++ static/js/scriptForDrawing.js
@@ -40,21 +40,24 @@ class ExactGraph {
 function drawGraph(graph) {
   var canvas = document.getElementById('canvasForGraph');
   var ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   // Ребра
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 2;
-  graph.edges.forEach(function(edge) {
+  // ctx.strokeStyle = 'black';
+  // ctx.lineWidth = 2;
+  // graph.edges.forEach(function(edge) {
+  //   ctx.beginPath();
+  //   ctx.moveTo(edge.a.x, edge.a.y);
+  //   ctx.lineTo(edge.b.x, edge.b.y);
+  //   ctx.stroke();
+  // });
+  // // Вершины
+  // ctx.fillStyle = 'blue';
+  // graph.vertices.forEach(function(vertex) {
+  //   ctx.beginPath();
+  //   ctx.arc(vertex.x, vertex.y, 5, 0, 2*Math.PI);
+  //   ctx.fill();
+  // });
     ctx.beginPath();
-    ctx.moveTo(edge.a.x, edge.a.y);
-    ctx.lineTo(edge.b.x, edge.b.y);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(50, 50, 100, 100);
     ctx.stroke();
-  });
-  // Вершины
-  ctx.fillStyle = 'blue';
-  graph.vertices.forEach(function(vertex) {
-    ctx.beginPath();
-    ctx.arc(vertex.x, vertex.y, 5, 0, 2*Math.PI);
-    ctx.fill();
-  });
 }
